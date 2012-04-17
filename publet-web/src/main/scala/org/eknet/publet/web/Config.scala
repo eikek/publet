eb656a9c2329aae711331b09d797e94559849c7b
Eike Kettner <eike.kettner@gmail.com>
2012-04-18 00:55:51 +0200

    add keySet method to Config

--- publet-web/src/main/scala/org/eknet/publet/web/Config.scala
+++ publet-web/src/main/scala/org/eknet/publet/web/Config.scala
@@ -1,8 +1,10 @@
 package org.eknet.publet.web
 
+import collection.JavaConversions._
 import java.util.Properties
 import java.io.{FileInputStream, File}
 import org.slf4j.LoggerFactory
+import System._
 
 /** Configuration file, is picked up from the configured publet directory. This is
  * either given as system property `publet.dir`, as environment variable `PUBLET_DIR`
@@ -15,9 +17,9 @@ import org.slf4j.LoggerFactory
 object Config {
   private val log = LoggerFactory.getLogger(getClass)
 
-  val directory = new File(Option(System.getProperty("publet.dir"))
-    .getOrElse(Option(System.getenv().get("PUBLET_DIR"))
-    .getOrElse(System.getProperty("user.home")+ File.separator +".publet")))
+  val directory = new File(Option(getProperty("publet.dir"))
+    .getOrElse(Option(getenv().get("PUBLET_DIR"))
+    .getOrElse(getProperty("user.home")+ File.separator +".publet")))
 
   if (!directory.exists()) if (!directory.mkdirs()) throw new RuntimeException("unable to create config dir: "+ directory)
   if (!directory.isDirectory) throw new RuntimeException("Config dir is not a directory: "+directory)
@@ -29,8 +31,9 @@ object Config {
   private val file = new File(directory, "publet.properties")
   private val props = new Properties(); reload();
 
-
   def value(key: String) = Option(props.getProperty(key))
+
+  def keySet = props.stringPropertyNames().toSet
 
   def reload() {
     synchronized {
