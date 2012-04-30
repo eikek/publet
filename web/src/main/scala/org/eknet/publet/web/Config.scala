f3c010759acd35007e7a0d89d5cee5b8a436f1f0
Eike Kettner <eike.kettner@gmail.com>
2012-04-30 07:46:27 +0000

    starting more verbose

--- web/src/main/scala/org/eknet/publet/web/Config.scala
+++ web/src/main/scala/org/eknet/publet/web/Config.scala
@@ -4,6 +4,7 @@ import java.io.{FileInputStream, File}
 import org.slf4j.LoggerFactory
 import System._
 import util.PropertiesMap
+import grizzled.slf4j.Logging
 
 /** Configuration file, is picked up from the configured publet directory. This is
  * either given as system property `publet.dir`, as environment variable `PUBLET_DIR`
@@ -16,8 +17,7 @@ import util.PropertiesMap
  * @since 17.04.12 09:30
  *
  */
-object Config extends PropertiesMap {
-  private val log = LoggerFactory.getLogger(getClass)
+object Config extends PropertiesMap with Logging {
 
   /**
    * The root config directory. Usually retrieved via `publet.dir` system
@@ -25,10 +25,18 @@ object Config extends PropertiesMap {
    *
    */
   val directory = {
-    val d = new File(Option(getProperty("publet.dir"))
-      .getOrElse(Option(getenv().get("PUBLET_DIR"))
-      .getOrElse(getProperty("user.home")+ File.separator +".publet")))
+    var dir = Option(getProperty("publet.dir"))
+    if (!dir.isDefined) {
+      info("System property 'publet.dir' not defined.")
+      dir = Option(getenv().get("PUBLET_DIR"))
+    }
+    if (!dir.isDefined) {
+      info("Environment variable 'PUBLET_DIR' not defined. Falling back to default.")
+      dir = Option(getProperty("user.home")+ File.separator +".publet")
+    }
 
+    info("Using publet directory: "+ dir.get)
+    val d = new File(dir.get)
     if (!d.exists()) if (!d.mkdirs()) throw new RuntimeException("unable to create config dir: "+ d)
     if (!d.isDirectory) throw new RuntimeException("Config dir is not a directory: "+d)
     d
@@ -44,7 +52,7 @@ object Config extends PropertiesMap {
    *
    */
   val configfile = new File(directory, "publet.properties")
-  log.info("Setup publet dir to: "+ directory)
+  info("Loading configuration file: "+ configfile)
   reload()
 
   /**
