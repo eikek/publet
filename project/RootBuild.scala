c476633aacb6e57061d9c8c4c273639a8965e99c
Eike Kettner <eike.kettner@gmail.com>
2012-06-30 22:44:42 +0200

    cleanup build file

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -5,49 +5,62 @@ import com.typesafe.sbtosgi.OsgiPlugin._
 import com.github.siasia._
 import WebappPlugin.webappSettings
 
-object Resolvers {
-  
-  val eknet = "eknet.org" at "http://maven.eknet.org/repo"
-  val ettrema = "ettrema.com" at "http://www.ettrema.com/maven2"
+object Version {
+  val osgi = "4.3"
+  val slf4j = "1.6.4"
+  val logback = "1.0.1"
+  val servlet = "2.5"
+  val cfileupload = "1.2.2"
+  val cio = "2.2"
+  val squaremail = "1.0.0"
+  val jgit = "1.3.0.201202151440-r"
+  val shiro = "1.2.0"
+  val scalaTest = "1.8"
+  val grizzled = "0.6.8"
+  val scalate = "1.5.3"
+  val mimeUtil = "2.1.3"
+  val orientdb = "1.0.1"
+  val blueprints = "2.0.0"
+  val milton = "1.8.0.1"
+  val ccodec = "1.4"
+  val jdom = "1.1"
 }
 
 object Dependencies {
 
-  val osgiCore = "org.osgi" % "org.osgi.core" % "4.3" withSources()
-  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.4"
-  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.0.1" withSources()
-  val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided" withSources()
+  val osgiCore = "org.osgi" % "org.osgi.core" % Version.osgi withSources()
+  val slf4jApi = "org.slf4j" % "slf4j-api" % Version.slf4j
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logback withSources()
+  val servletApi = "javax.servlet" % "servlet-api" % Version.servlet % "provided" withSources()
   val jettyContainer = "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "container" withSources()
-  val commonsFileUpload = "commons-fileupload" % "commons-fileupload" % "1.2.2"
-  val commonsIo = "commons-io" % "commons-io" % "2.2" withSources()
+  val commonsFileUpload = "commons-fileupload" % "commons-fileupload" % Version.cfileupload
+  val commonsIo = "commons-io" % "commons-io" % Version.cio withSources()
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % RootBuild.globalScalaVersion withSources()
-  val squareMail = "org.eknet.squaremail" % "squaremail" % "1.0.0"
-  val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "1.3.0.201202151440-r" withSources()
-  val jgitHttpServer = "org.eclipse.jgit" % "org.eclipse.jgit.http.server" % "1.3.0.201202151440-r" withSources()
-  val shiro = "org.apache.shiro" % "shiro-core" % "1.2.0" withSources()
-  val shiroWeb = "org.apache.shiro" % "shiro-web" % "1.2.0" withSources()
-  val scalaTest = "org.scalatest" %% "scalatest" % "1.8.RC1" % "test" withSources()
-  val grizzledSlf4j = "org.clapper" %% "grizzled-slf4j" % "0.6.8" withSources() //scala 2.9.1 only
+  val squareMail = "org.eknet.squaremail" % "squaremail" % Version.squaremail
+  val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % Version.jgit withSources()
+  val jgitHttpServer = "org.eclipse.jgit" % "org.eclipse.jgit.http.server" % Version.jgit withSources()
+  val shiro = "org.apache.shiro" % "shiro-core" % Version.shiro withSources()
+  val shiroWeb = "org.apache.shiro" % "shiro-web" % Version.shiro withSources()
+  val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest % "test" withSources()
+  val grizzledSlf4j = "org.clapper" %% "grizzled-slf4j" % Version.grizzled withSources() //scala 2.9.1 only
 
-  private val scalateVersion = "1.5.3"
-  val scalateWikitext = "org.fusesource.scalate" % "scalate-wikitext" % scalateVersion 
-  val scalateCore = "org.fusesource.scalate" % "scalate-core" % scalateVersion 
-  val scalatePage = "org.fusesource.scalate" % "scalate-page" % scalateVersion
+  val scalateWikitext = "org.fusesource.scalate" % "scalate-wikitext" % Version.scalate
+  val scalateCore = "org.fusesource.scalate" % "scalate-core" % Version.scalate
+  val scalatePage = "org.fusesource.scalate" % "scalate-page" % Version.scalate
 
-  val mimeUtil = "eu.medsea.mimeutil" % "mime-util" % "2.1.3" intransitive()
+  val mimeUtil = "eu.medsea.mimeutil" % "mime-util" % Version.mimeUtil intransitive()
 
-  val orientdbCore = "com.orientechnologies" % "orientdb-core" % "1.0.1" withSources()
-  val orientCommons = "com.orientechnologies" % "orient-commons" % "1.0.1" withSources()
-  val blueprintsCore = "com.tinkerpop.blueprints" % "blueprints-core" % "2.0.0" withSources() intransitive()
-  val blueprints = "com.tinkerpop.blueprints" % "blueprints-orient-graph" % "2.0.0" withSources() intransitive() //uses orientdb 1.0.1
+  val orientdbCore = "com.orientechnologies" % "orientdb-core" % Version.orientdb withSources()
+  val orientCommons = "com.orientechnologies" % "orient-commons" % Version.orientdb withSources()
+  val blueprintsCore = "com.tinkerpop.blueprints" % "blueprints-core" % Version.blueprints withSources() intransitive()
+  val blueprints = "com.tinkerpop.blueprints" % "blueprints-orient-graph" % Version.blueprints withSources() intransitive() //uses orientdb 1.0.1
 
-  val miltonApi = "com.ettrema" % "milton-api" % "1.8.0.1" withSources() intransitive()
+  val miltonApi = "com.ettrema" % "milton-api" % Version.milton withSources() intransitive() from("http://www.ettrema.com/maven2")
   val miltonApiDeps = Seq(
-    "commons-io" % "commons-io" % "1.4" withSources(),
-    "commons-codec" % "commons-codec" % "1.4" withSources(),
-    "org.jdom" % "jdom" % "1.1"
+    "commons-codec" % "commons-codec" % Version.ccodec withSources(),
+    "org.jdom" % "jdom" % Version.jdom
   )
-  val miltonServlet = "com.ettrema" % "milton-servlet" % "1.8.0.1" withSources() intransitive()
+  val miltonServlet = "com.ettrema" % "milton-servlet" % Version.milton withSources() intransitive() from("http://www.ettrema.com/maven2")
 
 }
 
@@ -60,8 +73,18 @@ object RootBuild extends Build {
     id = "root",
     base = file("."),
     settings = buildSettings
-  ) aggregate (Publet.module, Web.module, GitPart.module, GitrWeb.module, Auth.module,
-    ScalateEngine.module, War.module, WebEditor.module, Ext.module, Doc.module)
+  ) aggregate (
+      Publet.module,
+      Web.module,
+      GitPart.module,
+      GitrWeb.module,
+      Auth.module,
+      ScalateEngine.module,
+      War.module,
+      WebEditor.module,
+      Ext.module,
+      Doc.module
+    )
 
   val globalScalaVersion = "2.9.1"
 
@@ -75,7 +98,6 @@ object RootBuild extends Build {
     scalaVersion := globalScalaVersion,
     sbtPlugin := true,
     exportJars := true,
-    resolvers := Seq(Resolvers.eknet, Resolvers.ettrema),
     scalacOptions ++= Seq("-unchecked", "-deprecation")
   )
 
