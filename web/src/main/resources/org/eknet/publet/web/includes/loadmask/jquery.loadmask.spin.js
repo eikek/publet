68dc8781f8abc9f7ee562d3a894dad2aebfbcc7a
Eike Kettner <eike.kettner@gmail.com>
2012-09-25 22:11:25 +0200

    fixes to jquery.loadmask.spin plugin when div is larger than window
    the spinner would then be rendered below the visible canvas, if the
    element the spinner is attached to is larger than the visible canvas.

--- web/src/main/resources/org/eknet/publet/web/includes/loadmask/jquery.loadmask.spin.js
+++ web/src/main/resources/org/eknet/publet/web/includes/loadmask/jquery.loadmask.spin.js
@@ -1,11 +1,27 @@
 /**
  * Copyright (c) 2009 Sergiy Kovalchuk (serg472@gmail.com)
- * 
+ *
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- *  
+ *
  * Following code is based on Element.mask() implementation from ExtJS framework (http://extjs.com/)
  *
+ */
+/*
+ * Small changes:
+ * Copyright 2012 Eike Kettner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 ;(function($){
 	
@@ -82,7 +98,13 @@
 
 			if(options.overlaySize.width !== undefined)
 				maskDiv.width(options.overlaySize.width)
-		}
+		} else {
+            if (element.height() > window.innerHeight)
+                maskDiv.height(window.innerHeight);
+
+            if (element.width() > window.innerWidth)
+                maskDiv.width(window.innerWidth);
+        }
 		
 		// auto height fix for IE
 		if(navigator.userAgent.toLowerCase().indexOf("msie") > -1){
