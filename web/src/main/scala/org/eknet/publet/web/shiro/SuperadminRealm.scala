5ecc4e6b61e062be45aca8b72cef18fa2dd177d0
Eike Kettner <eike.kettner@gmail.com>
2012-11-11 16:31:29 +0100

    make superadmin realm authenticate using digest

--- web/src/main/scala/org/eknet/publet/web/shiro/SuperadminRealm.scala
+++ web/src/main/scala/org/eknet/publet/web/shiro/SuperadminRealm.scala
@@ -19,35 +19,65 @@ package org.eknet.publet.web.shiro
 import org.eknet.publet.auth.store.{PermissionStore, UserProperty, User, UserStoreAdapter}
 import org.eknet.publet.web.{ConfigReloadedEvent, Config}
 import com.google.inject.{Singleton, Inject}
-import org.eknet.publet.auth.DigestGenerator
+import org.eknet.publet.auth._
 import org.apache.shiro.realm.text.TextConfigurationRealm
 import com.google.common.eventbus.Subscribe
+import org.apache.shiro.authc.{SimpleAccount, UsernamePasswordToken, AuthenticationInfo, AuthenticationToken}
+import org.apache.shiro.authc.credential.SimpleCredentialsMatcher
+import org.apache.shiro.subject.PrincipalCollection
+import org.eknet.publet.web.ConfigReloadedEvent
+import org.apache.shiro.realm.AuthorizingRealm
 
 /**
  * @author Eike Kettner eike.kettner@gmail.com
  * @since 08.11.12 18:44
  */
 @Singleton
-class SuperadminRealm @Inject() (config: Config) extends TextConfigurationRealm {
+class SuperadminRealm @Inject() (config: Config) extends AuthorizingRealm {
 
-  setup()
+  private val login = "superadmin"
 
-  private def setup() {
-    users.clear()
-    roles.clear()
-    setUserDefinitions(null)
-    setRoleDefinitions(null)
-    if (config("superadminEnabled").getOrElse("true").toBoolean) {
-      setUserDefinitions("superadmin = "+ getPassword+ ", superuser")
-      setRoleDefinitions("superuser = *")
-      processDefinitions()
-    }
+  private var user = {
+    val pw = config("superadminPassword").getOrElse(login)
+    new User(login, Map(
+      UserProperty.password -> pw,
+      UserProperty.enabled -> config("superadminEnabled").getOrElse("true"),
+      UserProperty.digest -> DigestGenerator.encodePasswordInA1Format(login, "WebDav Area", pw),
+      UserProperty.fullName -> "Publet Superadmin"
+    ))
   }
 
-  private def getPassword = config("superadminPassword").getOrElse("superadmin")
+  setCredentialsMatcher(new CompositeCredentialsMatcher(List(
+    new DynamicHashCredentialsMatcher,
+    new DigestCredentialsMatcher,
+    new SimpleCredentialsMatcher
+  )))
+  setCachingEnabled(false)
+
 
   @Subscribe
   def reloadRealm(event: ConfigReloadedEvent) {
-    setup()
+    val map = this.user.properties + (UserProperty.enabled -> config("superadminEnabled").getOrElse("true"))
+    this.user = new User(user.login, map)
+  }
+
+  override def supports(token: AuthenticationToken) =
+    super.supports(token) || (token != null && token.isInstanceOf[DigestAuthenticationToken])
+
+  def doGetAuthenticationInfo(token: AuthenticationToken) = {
+    if (token.getPrincipal.toString == login) {
+      new UserAuthcInfo(user, getName)
+    } else {
+      null
+    }
+  }
+
+  def doGetAuthorizationInfo(principals: PrincipalCollection) = {
+    val principal = getAvailablePrincipal(principals).toString
+    if (principal == login) {
+      new PolicyAuthzInfo(user, Set(), Set("*"))
+    } else {
+      null
+    }
   }
 }
