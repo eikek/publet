40cd1c6e1b0ae37a84cf19a1f73e6efb08fd6d93
Eike Kettner <eike.kettner@gmail.com>
2013-04-10 21:01:05 +0200

    update git remote url for changed repository location
    if the installation moves to a new directory, the git partitions
    don't match with the remote repository anymore. this is now checked
    before commit or reset.

--- gitr/src/main/scala/org/eknet/gitr/Tandem.scala
+++ gitr/src/main/scala/org/eknet/gitr/Tandem.scala
@@ -17,25 +17,38 @@
 package org.eknet.gitr
 
 import org.eclipse.jgit.api.ResetCommand
+import grizzled.slf4j.Logging
 
 /**
  * @author Eike Kettner eike.kettner@gmail.com
  * @since 10.05.12 00:59
  */
-case class Tandem(name: RepositoryName, bare: GitrRepository, workTree: GitrRepository) {
+case class Tandem(name: RepositoryName, bare: GitrRepository, workTree: GitrRepository) extends Logging {
 
   def updateWorkTree() = {
     synchronized {
+      setupRemoteUrl()
       workTree.git.fetch().call()
       workTree.git.reset().setMode(ResetCommand.ResetType.HARD).setRef("origin/"+branch).call()
     }
   }
 
   def pushToBare() = synchronized {
+    setupRemoteUrl()
     workTree.git.push().call()
   }
 
   def branch = workTree.getBranch
+
+  private[this] def setupRemoteUrl() {
+    val remoteUrl = workTree.getConfig.getString("remote", "origin", "url")
+    val expected = bare.getDirectory.toURI.toString
+    if (remoteUrl != expected) {
+      info("Tandem repositories moved. Update remote url ...")
+      workTree.getConfig.setString("remote", "origin", "url", expected)
+      workTree.getConfig.save()
+    }
+  }
 
 }
 
